6c651acba751295738b142233d47ac8a2d7d15ad
Kamil Machanski <kamilmachanski@gmail.com>
2020-05-29 20:42:10 +0200

    data from the repository

--- RESTCrud/src/Controller/AnimalController.php
+++ RESTCrud/src/Controller/AnimalController.php
@@ -4,8 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Animal;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/animal")
@@ -13,45 +16,59 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnimalController extends AbstractController
 {
 
-    private const ANIMALS = [
-        [
-            'id' => 1,
-            'name' => 'reksio',
-            'biography' => 'alirtharth pira prhap thraeth pah paretp aert aerhtagvfaourg gawt ouagaeritgarefgi rg gr iag',
-            'race' => 'dog'
-        ],
-        [
-            'id' => 2,
-            'name' => 'buro',
-            'biography' => 'akurfuar ratgaeriyargf lragf ilare lhgarlt arelth gaerl traelg relt raelth ',
-            'race' => 'cat'
-        ]
-    ];
-
     /**
      * @Route("/", name="animal_list")
      */
-    public function list(){
-        return $this->json(self::ANIMALS);
+    public function list(Request $request){
+
+        $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(Animal::class);
+        $items = $repository->findAll();
+
+        return $this->json(
+            [
+                'limit' => $limit,
+                'data' => $items
+            ]
+
+        );
     }
 
     /**
-     * @Route("/{id}", name="animal_by_id", requirements={"id"="\d+"})
+     * @Route("/{id}", name="animal_by_id", requirements={"id"="\d+"}, methods={"GET"})
      */
     public function animalById($id){
         return $this->json(
-            self::ANIMALS[array_search($id, array_column(self::ANIMALS, 'id'))]
+            $this->getDoctrine()->getRepository(Animal::class)->find($id)
         );
     }
 
     /**
-     * @Route("/{name}", name="animal_by_name")
+     * @Route("/{name}", name="animal_by_name", methods={"GET"})
      */
     public function animalByName($name){
         return $this->json(
-            self::ANIMALS[array_search($name, array_column(self::ANIMALS, 'name'))]
+            $this->getDoctrine()->getRepository(Animal::class)->findOneBy(['name' => $name])
         );
     }
+
+    /**
+     * @Route("/add", name="animal_add", methods={"POST"})
+     */
+    public function addAnimal(Request $request){
+
+        /** @var Serializer $serializer */
+       $serializer = $this->get('serializer');
+
+       $animal = $serializer->deserialize($request->getContent(),Animal::class, 'json');
+
+       $em = $this->getDoctrine()->getManager();
+       $em->persist($animal);
+       $em->flush();
+
+       return $this->json($animal);
+    }
+
 
 
 }
